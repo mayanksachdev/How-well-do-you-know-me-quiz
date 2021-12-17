b0a2a4635daefb6f443f453d881b17e46a800cba
mayank sachdev <94102964+mayanksachdev@users.noreply.github.com>
2021-12-17 10:15:11 +0000

    refactor: changed the title of the quiz

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var chalk = require('chalk');
 var readlineSync = require('readline-sync');
 var userName = readlineSync.question(chalk.redBright('Please, enter your name: '))
-console.log(chalk.cyanBright('welcome, '  + chalk.redBright.bold.italic(userName) + ' "to the DO YOU KNOW ME?" quiz!'))
+console.log(chalk.cyanBright('welcome, '  + chalk.redBright.bold.italic(userName) + ' "to the HOW WELL DO YOU KNOW ME?" quiz!'))
+console.log(chalk.magenta.bgRedBright("let's playy!"))
 
 
