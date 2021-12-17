89b07680b60f01f166cd47f5860d7692b6034b7a
mayank sachdev <94102964+mayanksachdev@users.noreply.github.com>
2021-12-17 10:06:21 +0000

    feat: added a feature that takes the username and welcomes them

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var chalk = require('chalk');
 var readlineSync = require('readline-sync');
-
+var userName = readlineSync.question(chalk.redBright('Please, enter your name: '))
+console.log(chalk.cyanBright('welcome, '  + chalk.redBright.bold.italic(userName) + ' "to the DO YOU KNOW ME?" quiz!'))
 
 
