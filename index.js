f4af9fc7987c513a93b5f74508843e4b49a178eb
mayank sachdev <94102964+mayanksachdev@users.noreply.github.com>
2021-12-17 10:47:12 +0000

    feat: added an instruction manual

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var chalk = require('chalk');
 var readlineSync = require('readline-sync');
 var userName = readlineSync.question(chalk.redBright('Please, enter your name: '))
-console.log(chalk.cyanBright('welcome, '  + chalk.redBright.bold.italic(userName) + ' "to the HOW WELL DO YOU KNOW ME?" quiz!'))
+console.log(chalk.cyanBright('welcome, '  + chalk.magenta.bold.italic(userName) + ' "to the HOW WELL DO YOU KNOW ME?" quiz!'))
+console.log(chalk.blue('Please, read the instructions carefully!'))
+console.log(chalk.yellowBright('1. ' + 'this quiz comprises of 10 questions. '))
+console.log(chalk.yellowBright('2. ' + 'you will be rewarded 1 point for every correct answer. '))
+console.log(chalk.yellowBright('3. ' + 'there is no deduction for wrong answer.'))
+console.log(chalk.yellow('4. ' + chalk.bgWhite('You only need to enter the option number for responding!')))
+console.log('_________________________________________________________________')
 console.log(chalk.magenta.bgRedBright("let's playy!"))
+
+
 
 
